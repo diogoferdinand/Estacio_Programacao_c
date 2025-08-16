4c601d4cf387874ddf643a25f6ae8f3e0aadc964
diogoferdinand <diogoferdinand@gmail.com>
2025-08-16 00:51:23 +0000

    estacio

---
+++ input_and_out.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    float altura = 1.75;
+    char opcao = 'S';
+    char nome[20] = "Diogo";
+
+    printf("Sua idade é %d e seu nome é %s\n", idade , nome);
+    printf("sua altura é %f\n", altura);
+    printf(" A opção é %c\n\n", opcao);
+}
